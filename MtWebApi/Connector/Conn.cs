4498274e0ea1be81af13505a806b984b70f8d04f
nurullah <nurullahkarakoca@gmail.com>
2018-09-13 08:02:48 +0300

    İlk Web Api Denemem

---
+++ MtWebApi/Connector/Conn.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MtWebApi.Model;
+using Pomelo.Data.MySql;
+
+namespace MtWebApi.Connector
+{
+
+    public class Conn
+    {
+        private string connstring;
+        public Conn()
+        {
+            connstring = @"server=localhost;userid=root;password=pdaodq;database=Hospital"; //Connection string burada
+        }
+
+        public List<User> UserList()
+        {
+
+            List<User> allUser = new List<User>();
+
+
+            using (MySqlConnection connMysql = new MySqlConnection(connstring))
+            {
+
+                using (MySqlCommand cmdd = connMysql.CreateCommand())
+                {
+
+
+                    cmdd.CommandText = "Select * from userlist"; //Dönmesini istediğimiz veriler için Sql sorgumuzu buraya yazıyoruz.
+                    cmdd.CommandType = System.Data.CommandType.Text;
+
+                    cmdd.Connection = connMysql;
+
+                    connMysql.Open();
+
+                    using (MySqlDataReader reader = cmdd.ExecuteReader())
+                    {
+
+                        while (reader.Read())
+                        {
+                            allUser.Add(new User
+                            {
+                                id = reader.GetInt32(reader.GetOrdinal("id")) //veritabanından hangi alanları çekeceksek bu alanları ayarlıyoruz
+                                                                        ,
+                                name = reader.GetString(reader.GetOrdinal("name"))
+                                                                       ,
+                                pass = reader.GetString(reader.GetOrdinal("pass"))
+                            });
+                        }
+                    }
+                }
+
+                connMysql.Close();
+            }
+
+
+
+            return allUser;
+        }
+    }
+}
---
+++ MtWebApi/Model/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MtWebApi.Model
+{
+    public class User //veritabnında hangi tablodan veri cekeceksek o tablonun modellemesini burada yapıyoruz.
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string pass { get; set; }
+    }
+}
